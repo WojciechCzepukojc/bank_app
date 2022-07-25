623a7873123bee34d9cc67c6e7e9880f145bd770
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-07-25 14:08:59 +0200

    add save Customer test

--- src/main/java/com/example/bank_app/models/Customer.java
+++ src/main/java/com/example/bank_app/models/Customer.java
@@ -1,9 +1,6 @@
 package com.example.bank_app.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -12,6 +9,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 public class Customer {
 
--- src/test/java/com/example/bank_app/repositories/CustomersRepositoryTest.java
+++ src/test/java/com/example/bank_app/repositories/CustomersRepositoryTest.java
@@ -1,13 +1,40 @@
 package com.example.bank_app.repositories;
 
+import com.example.bank_app.models.Customer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CustomersRepositoryTest {
+
+    @Autowired
+    private CustomersRepository customersRepository;
+
     @Test
-    void test() {
+    void testSaveNewCustomerSucces() {
+        //given
+        Customer expectedCustomer = Customer.builder()
+                .name("Jurek")
+                .surname("Kiler")
+                .pesel("88062845876")
+                .build();
+        //when
+        Customer actualCustomer = customersRepository.save(expectedCustomer);
+        //then
+                assertNotNull(actualCustomer.getId());
+                assertEquals(expectedCustomer, actualCustomer);
+                assertEquals(expectedCustomer.getName(), actualCustomer.getName());
+                assertEquals(expectedCustomer.getSurname(), actualCustomer.getSurname());
+                assertEquals(expectedCustomer.getPesel(), actualCustomer.getPesel());
+
+    }
+
+    @Test
+    void testGetCustomerListSucces() {
+
     }
 }

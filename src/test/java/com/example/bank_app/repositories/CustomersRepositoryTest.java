5cd980eccc25de8f92b95936488b8fd8f157b4b6
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-07-25 12:42:46 +0200

    add test properties and configuration

---
+++ src/test/java/com/example/bank_app/repositories/CustomersRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.example.bank_app.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomersRepositoryTest {
+    @Test
+    void test() {
+    }
+}

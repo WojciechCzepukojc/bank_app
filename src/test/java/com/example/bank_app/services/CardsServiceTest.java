4675c362ff72b7cf24960f0bae2fdaef061045a1
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-07-27 20:48:32 +0200

    add testUpdateCard

--- src/test/java/com/example/bank_app/services/CardsServiceTest.java
+++ src/test/java/com/example/bank_app/services/CardsServiceTest.java
@@ -2,10 +2,13 @@ package com.example.bank_app.services;
 
 import com.example.bank_app.dto.CardDto;
 import com.example.bank_app.enums.CardType;
+import com.example.bank_app.exceptions.ResourceNotFoundException;
+import com.example.bank_app.exceptions.ResurceValidationException;
 import com.example.bank_app.mappers.CardsMapper;
 import com.example.bank_app.models.Account;
 import com.example.bank_app.models.Card;
 import com.example.bank_app.repositories.CardsRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,6 +19,8 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.time.LocalDate;
 import java.util.Optional;
+
+import static org.mockito.Mockito.times;
 
 
 @SpringBootTest
@@ -65,6 +70,56 @@ class CardsServiceTest {
         //then
         Mockito.verify(cardsRepository).findById(cardDto.getId());
         Mockito.doReturn(Optional.of(optionalCard)).when(cardsRepository).findById(1L);
+    }
+
+    @Test
+    void testUpdateCardSucces() {
+        //given
+        final Long id = cardDto.getId();
+        final Card expectedCard = cardsMapper.map(cardDto);
+
+        Mockito.when(cardsRepository.existsById(id)).thenReturn(true);
+
+        //when
+        cardsService.updateById(id, cardDto);
+
+        //then
+        Mockito.verify(cardsRepository).existsById(id);
+        Mockito.verify(cardsMapper, times(2)).map(cardDto);
+        Mockito.verify(cardsRepository).save(Mockito.any(Card.class));
+        Mockito.verifyNoMoreInteractions(cardsMapper, cardsRepository);
+    }
+
+    @Test
+    void testUpdateCardNotFound() {
+        //given
+        final Long id = cardDto.getId();
+
+        Mockito.when(cardsRepository.existsById(id)).thenReturn(false);
+
+        //when
+        Assertions.assertThrows(ResourceNotFoundException.class, ()->cardsService.updateById(id, cardDto));
+
+        //then
+        Mockito.verify(cardsRepository).existsById(id);
+        Mockito.verifyNoMoreInteractions(cardsMapper, cardsRepository);
+        Mockito.verifyNoInteractions(cardsMapper);
+    }
+
+    @Test
+    void testUpdateCardIdsConflict() {
+        //given
+        final Long id = 2L;
+
+        Mockito.when(cardsRepository.existsById(id)).thenReturn(true);
+
+        //when
+        Assertions.assertThrows(ResurceValidationException.class, ()->cardsService.updateById(id, cardDto));
+
+        //then
+        Mockito.verify(cardsRepository).existsById(id);
+        Mockito.verifyNoMoreInteractions(cardsMapper, cardsRepository);
+        Mockito.verifyNoInteractions(cardsMapper);
     }
 
 
